c9c56e6e29147ce1b21143d11a156390d68830c3
richod1 <frimpsup@gmail.com>
2024-02-08 15:14:32 +0000

    projectController added

--- server/controllers/ProjectController.js
+++ server/controllers/ProjectController.js
@@ -247,6 +247,188 @@ const inviteProjectMember = async (req, res, next) => {
 
 };
 
+const verifyInvitation=async(req,res,next)=>{
+    try{
+        const {projectid,userid,access,role}=req.query;
+        const code=req.params.code;
+        if(code===req.app.locals.CODE){
+            req.app.locals.CODE=null;
+            req.app.locals.resetSession=true;
+
+            const project=await ProjectModel.findById(projectid);
+            if(!project) return next(createError(404,"Project not found!"))
+            const user=await UserModel.findById(userid);
+        if(!user) {
+            return next(createError(404,"User not found!"))
+        }
+
+        for(let i=0;i<project.members.length;i++){
+            if(project.members[i].id===user.id){
+                return next(createError(403,"You are already a member of this project"))
+            }
+        }
+
+        const newMember={id:user.id,role:role,access:access}
+        const updatedProject=await ProjectModel.findByIdAndUpdate(projectid,{
+            $push:{members:newMember}
+        },{new:true});
+
+        UserModel.findByIdAndUpdate(user.id,{$push:{projects:updatedProject._id}},{new:true},(err,doc)=>{
+            if(err){
+                next(err)
+            }
+        })
+
+        res.status(200).json({message:"You have joined the ptoject Successfully"})
+        }
+        return res.status(201).json({message:"Invalid Link || Link expired!"})
+
+    }catch(err){
+        next(err)
+    }
+}
+
+const getProjectMembers=async(req,res,next)=>{
+    try{
+
+        const project=await ProjectModel.findById(req.params.id);
+        if(!project) return next(createError(404,"Project not found"));
+        res.status(200).json(project.members);
+
+    }catch(err){
+        next(err)
+    }
+}
+
+//add works to project
+export const addWork = async (req, res, next) => {
+    try {
+    const user = await UserModel.findById(req.user.id);
+    const project = await ProjectModel.findById(req.params.id);
+    if (!project) return next(createError(404, "Project not found!"));
+    for (let i = 0; i < project.members.length; i++) {
+        if (project.members[i].id.toString() === req.user.id) {
+        if (project.members[i].access === "Owner" || project.members[i].access === "Admin" || project.members[i].access === "Editor") {
+
+            console.log("tg");
+            //create tasks from the arrya of tasks or req.body.tasks and thne push the id of the tasks to the work
+
+            const tasks = [];
+            for (let i = 0; i < req.body.tasks.length; i++) {
+            const newTask = new TaskModel({
+                ...req.body.tasks[i],
+                task: req.body.tasks[i].task,
+                start_date: req.body.tasks[i].start_date,
+                end_date: req.body.tasks[i].end_date,
+                members: req.body.tasks[i].members,
+                projectId: project._id,
+            });
+            const task = await newTask.save();
+            tasks.push(task.id);
+            }
+            const newWork = new WorkModel({
+            ...req.body,
+            title: req.body.title,
+            desc: req.body.desc,
+            priority: req.body.priority,
+            tags: req.body.tags,
+            projectId: project._id,
+            creatorId: user.id,
+            tasks: tasks,
+            });
+            const work = await newWork.save();
+            console.log(work);
+            //add the work id to the tasks
+            for (let i = 0; i < tasks.length; i++) {
+            TaskModel.findByIdAndUpdate(tasks[i], { workId: newWork._id }, (err, doc) => {
+                if (err) {
+                next(err);
+                }
+            });
+            }
+            //add the work id to the project
+            const updatedProject = await ProjectModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: { works: work._id },
+            },
+            { new: true }
+            ).then(() => {
+              //add the work id to the members and the tasks
+            for (let k = 0; k < req.body.tasks.length; k++) {
+                for (let i = 0; i < req.body.tasks[k].members.length; i++) {
+                const user = UserModel.findByIdAndUpdate(req.body.tasks[k].members[i], { $push: { works: work._id } }, (err, doc) => {
+                    if (err) {
+                    next(err);
+                    }
+                });
+
+                for (let i = 0; i < tasks.length; i++) {
+                    UserModel.findByIdAndUpdate(req.body.tasks[k].members[i], { $push: { tasks: tasks[i] } }, (err, doc) => {
+                    if (err) {
+                        next(err);
+                    }
+                    }
+                    );
+                }
+                }
+
+            }
+            });
+            for (let k = 0; k < req.body.tasks.length; k++) {
+            for (let i = 0; i < req.body.tasks[k].members.length; i++) {
+                const newNotification = new Notification({
+                link: project.id,
+                type: "task",
+                message: `"${user.name}" added you to task "${req.body.tasks[k].task}" in work "${work.title}" in project "${project.title.toUpperCase()}".`,
+                });
+                const notification = await newNotification.save();
+
+                console.log(notification);
+                UserModel.findByIdAndUpdate(req
+                .body.tasks[k].members[i], { $push: { notifications: notification._id } }, (err, doc) => {
+                    if (err) {
+
+                    next(err);
+                    }
+                }
+                );
+            }
+            }
+
+            res.status(200).json(updateProject);
+        } else {
+            return res.status(403).send({
+            message: "You are not allowed to add works to this project"
+            });
+        }
+        }
+}
+    } catch (err) {
+    next(err);
+    }
+};
+
+const getWork=async(req,res,next)=>{
+    try{
+
+        const project =await ProjectModel.findById(req.params.id);
+        if(!project) return next(createError(404,"Project not found!"));
+        const works=await WorkModel.find({projectId:req.params.id}).populate("tasks").populate("createorId","name img")
+        .populate({
+            path:"tasks",
+            populate:{
+                path:"members",
+                select:"name img",
+            }
+        }).sort({createdAt:-1});
+        res.status(200).json(works)
+
+    }catch(err){
+        next(err)
+    }
+}
+
 module.exports={
     addProject,
     deleteProject,
@@ -255,4 +437,8 @@ module.exports={
     updateMembers,
     removeMember,
     inviteProjectMember,
+    verifyInvitation,
+    getProjectMembers,
+    addWork,
+    getWork,
 }
