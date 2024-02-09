b0ef00edc8a5aace3c72f256e99d7380f4373500
richod1 <frimpsup@gmail.com>
2024-02-09 23:09:58 +0000

    inviteTeamMember controller added

--- server/controllers/TeamController.js
+++ server/controllers/TeamController.js
@@ -188,6 +188,77 @@ const addTeamProject=async(req,res,next)=>{
     }
 }
 
+// nodemailer init
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASS
+    },
+    port:465,
+    host:'smtp.gmail.com'
+})
+
+export const inviteTeamMember = async (req, res, next) => {
+    //send mail using nodemailer
+
+    const user = await UserModel.findById(req.user.id);
+    if (!user) {
+        return next(createError(404, "User not found"));
+    }
+
+    const team = await TeamModel.findById(req.params.id);
+    if (!team) return next(createError(404, "Team not found!"));
+
+    req.app.locals.CODE = await otpGenerator.generate(8, { upperCaseAlphabets: true, specialChars: true, lowerCaseAlphabets: true, digits: true, });
+
+    const link = `${process.env.URL}/team/invite/${req.app.locals.CODE}?teamid=${req.params.id}&userid=${req.body.id}&access=${req.body.access}&role=${req.body.role}`;
+
+    const mailBody = `
+    <div style="font-family: Poppins, sans-serif; max-width: 600px; margin: 0 auto; background-color: #f9f9f9; padding: 20px; border: 1px solid #ccc; border-radius: 5px;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDHQMmI5x5qWbOrEuJuFWkSIBQoT_fFyoKOKYqOSoIvQ&s" alt="VEXA Logo" style="display: block; margin: 0 auto; max-width: 200px; margin-bottom: 20px;">
+  <h1 style="font-size: 22px; font-weight: 500; color: #854CE6; text-align: center; margin-bottom: 30px;">${team.name}</h1>
+  <div style="background-color: #FFF; border: 1px solid #e5e5e5; border-radius: 5px; box-shadow: 0px 3px 6px rgba(0,0,0,0.05);">
+    <div style="background-color: #854CE6; border-top-left-radius: 5px; border-top-right-radius: 5px; padding: 20px 0;">
+      <h2 style="font-size: 28px; font-weight: 500; color: #FFF; text-align: center; margin-bottom: 10px;">Invitation to Join Team: ${team.name}</h2>
+    </div>
+    <div style="padding: 30px;">
+      <p style="font-size: 16px; color: #666; margin-bottom: 20px;">Dear ${req.body.name},</p>
+      <p style="font-size: 16px; color: #666; margin-bottom: 20px;">You have been invited to join a team <b>${team.name}</b> on VEXA by <b>${user.name}</b>. Please follow the link below to accept the invitation:</p>
+      <div style="text-align: center;">
+        <a href=${link} style="background-color: #854CE6; color: #FFF; text-decoration: none; font-size: 16px; font-weight: 500; padding: 10px 30px; border-radius: 5px;">Accept Invitation</a>
+      </div>
+      <p style="font-size: 16px; color: #666; margin-top: 30px;">Best regards,</p>
+      <p style="font-size: 16px; color: #666;">The VEXA Team</p>
+    </div>
+  </div>
+</div>
+`;
+
+    for (let i = 0; i < team.members.length; i++) {
+        if (team.members[i].id.toString() === req.user.id) {
+            if (team.members[i].access === "Owner" || team.members[i].access === "Admin" || team.members[i].access === "Editor") {
+
+                const mailOptions = {
+                    from: process.env.EMAIL,
+                    to: req.body.email,
+                    subject: `Invitation to join team ${team.name}`,
+                    html: mailBody
+                };
+                transporter.sendMail(mailOptions, (err, data) => {
+                    if (err) {
+                        return next(err);
+                    } else {
+                        return res.status(200).json({ message: "Email sent successfully" });
+                    }
+                });
+            } else {
+                return next(createError(403, "You are not allowed to invite members to this project!"));
+            }
+        }
+    }
+};
+
 module.exports={
     addTeam,
     deleteTeam,
@@ -196,4 +267,5 @@ module.exports={
     updateMembers,
     removeMember,
     addTeamProject,
+    inviteTeamMember,
 }
