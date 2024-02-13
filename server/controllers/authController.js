d7c56ff6b7858bc6dbd5135a647f6be6b5b1077d
richod1 <frimpsup@gmail.com>
2024-02-13 17:07:05 +0000

    google auth added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,7 +28,7 @@ const signUp=async(req,res,next)=>{
         }
         const salt=bcrypt.genSaltSync(10);
         const hashedPassword=bcrypt.hashSync(req.bocy.password,salt);
-        const newUser=new UserMOdel({...req.body,password:hashedPassword});
+        const newUser=new UserModel({...req.body,password:hashedPassword});
 
         newUser.save().then((user)=>{
             const token=jwt.sign({id:user._id},process.env.SECRET_KEY,{expiresIn:"10d"})
@@ -40,4 +40,63 @@ const signUp=async(req,res,next)=>{
     }catch(err){
         next(err)
     }
+}
+
+const signIn=async(req,res,next)=>{
+    const {email}=req.body.email;
+    try{
+        const user=await UserModel.findOne({email});
+        if(!user){
+            return next(createError(404,"User not found!"))
+        }
+        if(user.googleSignIn){
+            return next(createError(201,"Entered Email is singned upn with google account.Please sign in with google"))
+        }
+        const validPassword=await bcrypt.compareSync(req.body.password,user.password);
+        if(!validPassword){
+            return next(createError(201,"Wrong password"));
+        }
+
+// create token for user
+const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"10d"});
+res.status(200).json({token,user})
+
+    }catch(err){
+    next(err);
+
+    }
+
+}
+
+
+// google signIn
+const googleAuthSignIn=async(req,res,next)=>{
+    const {email}=req.body.email;
+    try{
+        const user=await UserModel.findOne({email})
+        if(!user){
+            try{
+                const user=new UserModel({...req.body,googleSignIn:true});
+                await user.save();
+                const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"10d"});
+                res.status(200).json({token,user:user});
+            }catch(err){
+                next(err)
+            }
+        }else if(user.googleSignIn){
+            const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"10d"})
+            res.status(200).json({token,user});
+        }else if(user.gogleSignIn==false){
+            return next(createError(201,"User already exist with this email"))
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
+
+module.exports={
+    signUp,
+    signIn,
+    googleAuthSignIn,
 }
